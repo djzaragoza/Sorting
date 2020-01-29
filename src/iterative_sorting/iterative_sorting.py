b95fa30f780beb4ab865f7bd3234ae4d40080530
unknown <dj.zaragoza@icloud.com>
2020-01-28 21:12:38 -0800

    finished bubble sort algorithm

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -20,8 +20,21 @@ def selection_sort( arr ): #insertion sort algorithm
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    x = len(arr)
+    for i in range(x):
+        for y in range(0, x-i-1):
+            if arr[z] > arr[z+1]:
+                arr[z], arr[z+1] = arr[z+1], arr[z]
     
     return arr
+
+arr = [75, 43, 12, 84, 5, 18]
+
+bubble_sort(arr)
+
+print("Sorted array is:")
+for i in range(len(arr)):
+    print("%d" %arr[i]),
 
 
 # STRETCH: implement the Count Sort function below
